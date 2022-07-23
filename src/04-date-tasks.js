c0b402971092ad3681714777fc9add1807ec30e5
Vladimir Kozinsky <kozinskytxc@gmail.com>
2022-07-23 12:54:23 +0400

    feat: add 04-date-tasks solutions

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,14 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  if (year === 1900) return false;
+
+  if (+year % 4 === 0) {
+    return true;
+  }
+  return false;
 }
 
 
@@ -73,8 +79,12 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const date = new Date(null);
+  const seconds = endDate - startDate;
+  date.setMilliseconds(seconds); // specify value for SECONDS here
+  const result = date.toISOString().substr(11, 12);
+  return result;
 }
 
 
@@ -94,8 +104,19 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  // eslint-disable-next-line no-console
+  const hours = date.getUTCHours();
+  const minutes = date.getMinutes();
+
+  const hour = hours > 12 ? hours - 12 : hours;
+  const minutePos = minutes * 6;
+  const hourPos = minutePos > 0 ? (hour * 30) + (minutePos / 12) : hour * 30;
+  const angle = hourPos > minutePos ? hourPos - minutePos : minutePos - hourPos;
+
+  // eslint-disable-next-line no-mixed-operators
+  const rad = angle > 180 ? (angle - 180) * Math.PI / 180 : angle * Math.PI / 180;
+  return rad;
 }
 
 
